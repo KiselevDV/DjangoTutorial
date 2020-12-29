23407d7ac5364cc7cd962790d95bf11215b8fbc1
Dmitry Kiselev <kiselevdv@mediatech.dev>
2020-12-29 22:52:28 +0300

    part 7: Writing your first Django app

--- polls/admin.py
+++ polls/admin.py
@@ -2,5 +2,28 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 1
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ('pub_date',)
+    search_fields = ('question_text',)
+    inlines = [ChoiceInline]
+    fieldsets = [
+        (None, {
+            'fields': ('question_text',)
+        }),
+        ('Date information', {
+            'fields': ('pub_date',)
+        })
+    ]
+
+
+@admin.register(Choice)
+class ChoiceAdmin(admin.ModelAdmin):
+    fields = ['question', 'choice_text', 'votes']
